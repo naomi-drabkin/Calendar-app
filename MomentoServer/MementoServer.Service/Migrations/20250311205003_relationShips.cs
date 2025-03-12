dec0a9a2451a777b66b63b3ebb41e7748bff76bb
naomi-drabkin <nd055670041@gmail.com>
2025-03-12 22:06:05 +0200

    updates for render

---
+++ MomentoServer/MementoServer.Service/Migrations/20250311205003_relationShips.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MementoServer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class relationShips : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Templates",
+                newName: "PdfUrl");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Templates_UserId",
+                table: "Templates",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_UserId",
+                table: "Images",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calendars_UserId",
+                table: "Calendars",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Calendars_Users_UserId",
+                table: "Calendars",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Templates_Users_UserId",
+                table: "Templates",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Calendars_Users_UserId",
+                table: "Calendars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Templates_Users_UserId",
+                table: "Templates");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Templates_UserId",
+                table: "Templates");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_UserId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Calendars_UserId",
+                table: "Calendars");
+
+            migrationBuilder.RenameColumn(
+                name: "PdfUrl",
+                table: "Templates",
+                newName: "ImageUrl");
+        }
+    }
+}
