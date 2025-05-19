7f0d1c31af5b5a4a5ab813adece0fde07165a4cf
naomi-drabkin <nd055670041@gmail.com>
2025-05-20 00:38:50 +0300

    update cors

--- MomentoServer/MomentoServer/Program.cs
+++ MomentoServer/MomentoServer/Program.cs
@@ -218,7 +218,7 @@ app.MapGet("/", () => "server is running");
 
 
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+app.UseCors("AllowClients");
 
 //app.UseAuthorization();
 app.UseAuthentication();
